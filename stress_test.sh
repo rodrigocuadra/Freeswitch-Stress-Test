820cd24b8512aa163fd525f3512c9743a5098f8f
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-05-22 12:27:05 -0600

    Update stress_test.sh

--- stress_test.sh
+++ stress_test.sh
@@ -96,12 +96,15 @@ fs_cli -x 'reload mod_sofia' >/dev/null
 # -------------------------------------------------------------
 echo -e "Creating dialplan for 9500 on remote server..."
 
-ssh -p "$ssh_remote_port" root@$ip_remote "cat <<EOF > /etc/freeswitch/dialplan/public/9500.xml
-<extension name=\"moh-test\">
-  <condition field=\"destination_number\" expression=\"^9500$\">
-    <action application=\"answer\"/>
-    <action application=\"playback\" data=\"local_stream://moh\"/>
-    <action application=\"hangup\"/>
+ssh -p "$ssh_remote_port" root@$ip_remote "mkdir -p /etc/freeswitch/dialplan/stress-test"
+
+# ssh -p "$ssh_remote_port" root@$ip_remote "cat <<EOF > /etc/freeswitch/dialplan/public/9500.xml
+ssh -p "$ssh_remote_port" root@$ip_remote "cat <<EOF > /etc/freeswitch/dialplan/stress-test/9500.xml
+<extension name="stress-test-sleep">
+  <condition field="destination_number" expression="^9500$">
+    <action application="answer"/>
+    <action application="sleep" data="180000"/>
+    <action application="hangup"/>
   </condition>
 </extension>
 EOF"
